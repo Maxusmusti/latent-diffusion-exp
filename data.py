bb53b332d716ded24c5e799563af6a17f8e98dd8
jjlee0802cu <jjl2245@columbia.edu>
2023-04-16 15:24:30 -0400

    Add preprocessing and quality filteres for LAION-5b dataset

--- data.py
+++ data.py
@@ -21,19 +21,47 @@ import requests
 import PIL
 from PIL import Image
 import time
+import torchvision.transforms as T
+
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_STD = (0.229, 0.224, 0.225)
 
 
 def load_image_from_url(URL):
     """
-    Loads an image from URL using requests package and BytesIO package
+    Loads an image from URL using requests package and BytesIO package. Returns image as tensor
+        Performs preprocessing so that the channels are normalized and image is resized to 224x224.
+        To normalize, use mean and standard deviation from the ImageNet as a baseline.
+        To resize to 224x224, image is upscaled or downscaled and then cropped down to the center 224x224 pixels
     URL: the URL from which to load the image
     """
     try:
         call = requests.get(URL, timeout=5) # timeout of 5 seconds so we don't hang indefinitely
-        return np.array(Image.open(BytesIO(call.content)))
+        image = Image.open(BytesIO(call.content))
+        preprocess = T.Compose([
+            T.Resize(224),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            T.Normalize(
+                mean=IMAGENET_MEAN,
+                std=IMAGENET_STD
+            )
+        ])
+        return preprocess(image)
     except:
         return None
     
+    
+def quality_filters(x):
+    """
+    Returns whether the entry in the dataset meets quality filters: no watermark and is sfw
+        Threshold for watermark: 0.8
+        Threshold for nsfw: 0.5
+    x: Row in dataset
+    """
+    return x["pwatermark"] is not None and x["pwatermark"] < 0.8 and x["punsafe"] is not None and x["punsafe"] < 0.5
+
 
 def get_dataset(path):
     """
@@ -41,6 +69,7 @@ def get_dataset(path):
     path: the HuggingFace path following https://huggingface.co/datasets/
     """
     data = HuggingFaceHubReader(path) # returns an iterable HuggingFace dataset
+    data = data.filter(quality_filters) # filters out images with watermark and images that are unsafe
     data = data.shuffle().sharding_filter() # allows DataPipe to be sharded
     data = data.slice(index=["TEXT", "URL"]) # get columns by index
     data = data.map(fn=load_image_from_url, input_col="URL", output_col="IMAGE") # load each image and put it in a new "IMAGE" column
@@ -50,6 +79,7 @@ def get_dataset(path):
     return data
 
 
+# TODO: split this data loader into 85% train, 5% validation, 10% test
 def get_data_loader(path):
     """
     Creates a data loader from the given HuggingFace path
@@ -61,17 +91,27 @@ def get_data_loader(path):
     return data_loader
 
 
+def unnormalize_tensor(tensor, mean, std):
+    """
+    Un-normalizes a tensor with given mean and standard-deviation. (For displaying purposes)
+    """
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
+    
+
 def view_entry(entry, debug=False):
     if not debug:
         return
     
-    label, image = entry["TEXT"], entry["IMAGE"]
+    label, tensor_image = entry["TEXT"], entry["IMAGE"]
     try:
         print("\t\t", label)
-        plt.imshow(image)
+        tensor_to_image = T.Compose([T.ToPILImage()])
+        plt.imshow(tensor_to_image(unnormalize_tensor(tensor_image, IMAGENET_MEAN, IMAGENET_STD)))
         plt.savefig('./image.png')
         plt.clf()
-        time.sleep(0.25)
+        time.sleep(0.2)
     except PIL.UnidentifiedImageError:
         print("corrupted")
 
@@ -83,4 +123,4 @@ if __name__ == "__main__":
         print("Batch", i)
         for j, entry in enumerate(batch):
             print("\tEntry", j)
-            view_entry(entry, debug=False)
+            view_entry(entry, debug=True)
